40c8acf97a2a1e8b205f698d810fac7ce3935dec
Eduard Schumacher <107512513+mijamoto911@users.noreply.github.com>
2024-10-13 22:56:58 +0200

    fix code

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import json from '@rollup/plugin-json';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), json()],
+  plugins: [react()],
 });
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import json from '@rollup/plugin-json';
+
+// export default defineConfig({
+//   plugins: [react(), json()],
+// });
